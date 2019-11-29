35af16254d97d03af5707d697ba156ccbda4bcc4
Mille737 <mille737@gmail.com>
2019-11-29 14:39:21 +0100

    fields, getter, setter

--- src/main/java/com/eksamen/gokart/Model/Booking.java
+++ src/main/java/com/eksamen/gokart/Model/Booking.java
@@ -4,5 +4,55 @@ import java.sql.*;
 public class Booking {
     Date dato;
     Time tid;
+    int pax;
+    int varighed;
+    String kommentar;
 
+    public Booking(Date dato, Time tid, int PAX, int varighed, String kommentar) {
+        this.dato = dato;
+        this.tid = tid;
+        this.pax = pax;
+        this.varighed = varighed;
+        this.kommentar = kommentar;
+    }
+
+    public Date getDato() {
+        return dato;
+    }
+
+    public void setDato(Date dato) {
+        this.dato = dato;
+    }
+
+    public Time getTid() {
+        return tid;
+    }
+
+    public void setTid(Time tid) {
+        this.tid = tid;
+    }
+
+    public int getpax() {
+        return pax;
+    }
+
+    public void setpax(int PAX) {
+        this.pax = pax;
+    }
+
+    public int getVarighed() {
+        return varighed;
+    }
+
+    public void setVarighed(int varighed) {
+        this.varighed = varighed;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
 }
