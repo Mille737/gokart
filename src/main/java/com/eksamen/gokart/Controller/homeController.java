d3f1b39b7a15c8280c1350ae2dfda6f1daa342a9
Nadia Hansen <nadiahansen.kea@gmail.com>
2019-11-29 13:53:46 +0100

    tst
    tst

---
+++ src/main/java/com/eksamen/gokart/Controller/homeController.java
@@ -0,0 +1,17 @@
+package com.eksamen.gokart.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+    @RequestMapping (value = "/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping (value = "/home")
+    public String homeLogo() {
+        return "home";
+    }
+}
