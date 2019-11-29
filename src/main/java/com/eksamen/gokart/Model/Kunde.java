2a92a57c1aa1099e6a712515ad3e9d8064ae1448
Mille737 <mille737@gmail.com>
2019-11-29 13:45:07 +0100

    model layer

---
+++ src/main/java/com/eksamen/gokart/Model/Kunde.java
@@ -0,0 +1,47 @@
+package com.eksamen.gokart.Model;
+
+public class Kunde {
+    String navn;
+    String firma;
+    String email;
+    int nummer;
+
+    public Kunde(String navn, String firma, String email, int nummer) {
+        this.navn = navn;
+        this.firma = firma;
+        this.email = email;
+        this.nummer = nummer;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        this.nummer = nummer;
+    }
+}
